2f71e108976b0a1da4da973f1df56ff1bc491f1c
Sayeed Shorif <kaihiwataridb07@gmail.com>
2024-04-23 20:23:12 +0600

    new commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./frontEndProjectsUsing-HTML-TAILWINDCSS-JAVASCRIPT/public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
